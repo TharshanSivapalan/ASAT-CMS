2a21559b58f62cb010e551849e5f43e40e7e9c63
ayuladio <armelyuladio@gmail.com>
2017-07-12 23:38:40 +0200

    Correction page choix du theme

--- app/views/theme-list.view.php
+++ app/views/theme-list.view.php
@@ -4,17 +4,25 @@
     </div>
 
     <div class="onglet-contenu" >
-        
-        <?php foreach ($list_template as $template) : ?>
+        <div class="theme-block">
+            <?php foreach ($list_template as $template) : ?>
+                <div class="theme-block-content">
+                    <h2><?php echo $template['name']?></h2>
+                <?php if ($template['statut'] == 1) {
+                        echo "<span>Activé</span>";
+                    } else {
+                        echo "<a href='/theme/select/". $template['id'] ."'>Choisir</a>";
+                    }
 
-            <h2><?php echo $template['name']?></h2>
-            <?php if ($template['statut'] == 1) : ?>
-                <span>Activé</span>
-            <?php endif; ?>
-            <img src="/img/<?php echo $template['thumbnail']?>" alt="thumbnail <?php echo $template['name']?>" style="width: 400px">
-            <a href="/theme/select/<?php echo $template['id']?>">Choisir</a>
+                ?>
+                    
+                <img src="/img/<?php echo $template['thumbnail']?>" alt="thumbnail <?php echo $template['name']?>" >
+                
 
-        <?php endforeach; ?>
+                </div>
+                
 
+            <?php endforeach; ?>
+        </div>
     </div>
 </div>
