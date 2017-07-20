094d61a224f1f5ec64b57a4e5ee906fad687f5e1
shivasrajah <shiva.srajah@gmail.com>
2017-07-20 12:44:29 +0200

    Changement mineur fichier core View.class.php & Installer

--- app/core/View.class.php
+++ app/core/View.class.php
@@ -1,7 +1,7 @@
 <?php 
 
     class View {
-        
+
         protected $view;
         protected $template;
         protected $data = [];
@@ -16,11 +16,17 @@
         }
 
         public function setView ($view) {
-            
-            if(file_exists("../app/views/".$view.".view.php") ){
-                $this->view = "../app/views/".$view.".view.php";
+
+            $uri = $_SERVER["REQUEST_URI"];
+            $uri = preg_replace("#".BASE_PATH_PATTERN."#i", "", $uri, 1);
+            $uri = (explode("/",  trim($uri, "/")   ));
+            if(file_exists("../app/views/".$uri[0]."/".$view.".view.php") || file_exists("../app/views/".$view.".view.php")){
+                if(file_exists("../app/views/".$uri[0]."/".$view.".view.php")){
+                    $this->view = "../app/views/".$uri[0]."/".$view.".view.php";
+                }else{
+                    $this->view = "../app/views/".$view.".view.php";
+                }
             }
-            
             else {
                 
                 die("La vue n'existe pas");
@@ -29,13 +35,17 @@
 
         public function setTemplate ($template) {
 
+            $uri = $_SERVER["REQUEST_URI"];
+            $uri = preg_replace("#".BASE_PATH_PATTERN."#i", "", $uri, 1);
+            $uri = (explode("/",  trim($uri, "/")   ));
 
-
-            if(file_exists("../app/views/".$template.".temp.php") ){
-                $this->template = $template.".temp.php";
-            }
-
-            else {
+            if(file_exists("../app/views/".$uri[0]."/".$template.".temp.php") || file_exists("../app/views/".$template.".temp.php")){
+                if(file_exists("../app/views/".$uri[0]."/".$template.".temp.php")){
+                    $this->template = "../app/views/".$uri[0]."/".$template.".temp.php";
+                }else{
+                    $this->template = "../app/views/".$template.".temp.php";
+                } 
+           }else {
 
                 die("Le template n'existe pas");
             }
@@ -82,7 +92,7 @@
         public function __destruct () {
             
             extract($this->data);
-            include "../app/views/".$this->template;
+            include $this->template;
         }
 
     }
