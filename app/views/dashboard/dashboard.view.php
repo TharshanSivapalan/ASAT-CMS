a3241865c2cf8de93b8c70954b99adde4c744272
ayuladio <armelyuladio@gmail.com>
2017-07-25 15:55:00 +0200

    modif dashboard

--- app/views/dashboard/dashboard.view.php
+++ app/views/dashboard/dashboard.view.php
@@ -3,20 +3,22 @@
         <h3>Dashboard</h3>
     </div>
 
-    <div class="onglet-contenu" >
-        <div class="theme-block">
-	      <div id="nbVisiteStart">70</div>
-	      <div id="nbVisiteFinish">30</div>
-    	</div>
-
-    <div id="chartdiv" style="width: 100%; height: 355px;">#Donuts</div>
-	  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-	  integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
-	  crossorigin="anonymous"></script>
-	  <script src="https://www.amcharts.com/lib/3/amcharts.js"></script>
-	  <script src="https://www.amcharts.com/lib/3/pie.js"></script>
-	  <script src="https://www.amcharts.com/lib/3/themes/none.js"></script>
+    <div class="onglet-contenu dashboard-top" >
+	      <div class="dashboard-top-item" ><span class="dashboard-title-item">Nombre de visite total</span><span class="dashboard-nb-item" id="nbVisiteStart" >70</span></div>
+	      <div id="nbVisiteFinish" class="dashboard-top-item"><span class="dashboard-title-item"></span><span class="dashboard-nb-item"  id="nbVisiteFinish"  >70</span></div>
+	      <div id="nbVisiteFinish" class="dashboard-top-item"><span class="dashboard-title-item"></span><span class="dashboard-nb-item" >70</span></div>
     </div>
+
+    <div class="onglet-contenu dashboard-bottom" >
+	    <div id="chartdiv" style="width: 100%; height: 355px;">#Donuts</div>
+		  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+		  integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
+		  crossorigin="anonymous"></script>
+		  <script src="https://www.amcharts.com/lib/3/amcharts.js"></script>
+		  <script src="https://www.amcharts.com/lib/3/pie.js"></script>
+		  <script src="https://www.amcharts.com/lib/3/themes/none.js"></script>
+	    </div>
+	</div>
 
     <script>
 		$nbVisiteFinish = $("#nbVisiteFinish").text();
